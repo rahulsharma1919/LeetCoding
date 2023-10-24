368582a97fed76fe1594558914a085acc1458840
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2023-10-24 19:21:09 +0530

    Time: 5 ms (92.91%), Space: 11.2 MB (25.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,30 +1,34 @@
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i<nums.size(); i++){
-            for (int j=i+1; j<nums.size(); j++){
-                if (nums[i] + nums[j] == target){
-                    return {i,j};
-                }
-            }
-        }
-        return {};
-    }
-};
 // class Solution {
 // public:
 //     vector<int> twoSum(vector<int>& nums, int target) {
-//         unordered_map<int, int> numMap;
-//         int n = nums.size();
-
-//         for (int i = 0; i < n; i++) {
-//             int complement = target - nums[i];
-//             if (numMap.count(complement)) {
-//                 return {numMap[complement], i};
+//         // traversing till the last element
+//         for (int i = 0; i<nums.size(); i++){
+//             // another loop from i+1 till the last element
+//             for (int j=i+1; j<nums.size(); j++){
+//                 // check if pair's sum is equal to target or not 
+//                 if (nums[i] + nums[j] == target){
+//                     // returning the indices if true 
+//                     return {i,j};
+//                 }
 //             }
-//             numMap[nums[i]] = i;
-//         }
-
-//         return {}; // No solution found
+//         }   //brute force method
+//         return {};  // no solution found
 //     }
 // };
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
+            }
+            numMap[nums[i]] = i;
+        }
+
+        return {}; // No solution found
+    }
+};
