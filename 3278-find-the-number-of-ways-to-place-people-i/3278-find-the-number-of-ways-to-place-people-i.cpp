59fceda7c89b13509c300c0b610d0500c55b0acd
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-09-02 13:11:45 +0530

    Time: 23 ms (25.93%), Space: 32.4 MB (88.89%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
-        int count = 0;
+        int ans = 0;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; ++i) {
+            int x1 = points[i][0], y1 = points[i][1];
+            for (int j = 0; j < n; ++j) {
                 if (i == j) continue;
-                auto A = points[i];
-                auto B = points[j];
+                int x2 = points[j][0], y2 = points[j][1];
                 
-                // Check if A is upper-left of B
-                if (A[0] <= B[0] && A[1] >= B[1]) {
+                // A is on the "upper-left side" of B:
+                // allow same x or same y (line is allowed), but they must be different points
+                if (x1 <= x2 && y1 >= y2) {
                     bool valid = true;
-                    for (int k = 0; k < n; k++) {
+                    // Check every other point: it must NOT lie inside or on the border
+                    for (int k = 0; k < n; ++k) {
                         if (k == i || k == j) continue;
-                        auto P = points[k];
-                        if (A[0] <= P[0] && P[0] <= B[0] &&
-                            B[1] <= P[1] && P[1] <= A[1]) {
+                        int x = points[k][0], y = points[k][1];
+                        if (x1 <= x && x <= x2 && y2 <= y && y <= y1) {
                             valid = false;
                             break;
                         }
                     }
-                    if (valid) count++;
+                    if (valid) ++ans;
                 }
             }
         }
         
-        return count;
+        return ans;
     }
 };
