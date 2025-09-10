bae3d9fe248bee207476ce28b5bd43c72e71528b
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-09-10 12:40:04 +0530

    Time: 215 ms (56.47%), Space: 148 MB (29.41%) - LeetHub

--- 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/1834-minimum-number-of-people-to-teach.cpp
@@ -2,47 +2,27 @@ class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
         int m = languages.size();
-        
-        // convert languages[i] into sets for faster lookup
         vector<unordered_set<int>> knows(m + 1);
-        for (int i = 0; i < m; i++) {
-            for (int lang : languages[i]) {
-                knows[i + 1].insert(lang);
-            }
-        }
+        for (int i = 0; i < m; i++)
+            knows[i + 1] = unordered_set<int>(languages[i].begin(), languages[i].end());
 
-        // find users who are in "broken friendships" (cannot talk)
         unordered_set<int> needTeach;
         for (auto &f : friendships) {
             int u = f[0], v = f[1];
-            bool canCommunicate = false;
+            bool ok = false;
             for (int lang : knows[u]) {
-                if (knows[v].count(lang)) {
-                    canCommunicate = true;
-                    break;
-                }
+                if (knows[v].count(lang)) { ok = true; break; }
             }
-            if (!canCommunicate) {
-                needTeach.insert(u);
-                needTeach.insert(v);
-            }
+            if (!ok) { needTeach.insert(u); needTeach.insert(v); }
         }
 
-        // if no one needs teaching
         if (needTeach.empty()) return 0;
 
-        // try teaching each language and calculate minimum needed
-        int ans = INT_MAX;
-        for (int lang = 1; lang <= n; lang++) {
-            int cnt = 0;
-            for (int user : needTeach) {
-                if (!knows[user].count(lang)) {
-                    cnt++;
-                }
-            }
-            ans = min(ans, cnt);
-        }
+        vector<int> freq(n + 1, 0);
+        for (int user : needTeach)
+            for (int lang : knows[user]) freq[lang]++;
 
-        return ans;
+        int best = *max_element(freq.begin(), freq.end());
+        return (int)needTeach.size() - best;
     }
 };
