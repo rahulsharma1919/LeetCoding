1975dc0664302606a6def6dab62fc7ceb466cf95
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-10-15 16:03:12 +0530

    Time: 241 ms (30.83%), Space: 173.7 MB (81.58%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -2,32 +2,21 @@ class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        vector<int> incEnd(n, 1);
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > nums[i - 1])
-                incEnd[i] = incEnd[i - 1] + 1;
-            else
-                incEnd[i] = 1;
-        }
-        auto ok = [&](int k) -> bool {
-            if (k == 0)
-                return true;
-            if (2 * k > n)
-                return false;
-            for (int a = 0; a + 2 * k - 1 < n; ++a) {
-                if (incEnd[a + k - 1] >= k && incEnd[a + 2 * k - 1] >= k)
-                    return true;
+        int curr = 1, prev = 0, ans = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                curr++;
+            } else {
+                prev = curr;  
+                curr = 1;     
             }
-            return false;
-        };
-        int lo = 0, hi = n / 2;
-        while (lo < hi) {
-            int mid = (lo + hi + 1) >> 1;
-            if (ok(mid))
-                lo = mid;
-            else
-                hi = mid - 1;
+
+            int half = curr / 2;              
+            int adjacent = min(prev, curr);   
+            ans = max(ans, max(half, adjacent));
         }
-        return lo;
+
+        return ans;
     }
 };
