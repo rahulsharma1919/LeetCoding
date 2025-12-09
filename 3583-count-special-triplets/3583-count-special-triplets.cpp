ac5f1a81b089701f5241f36d725bbce87d7c7c80
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2025-12-09 13:26:10 +0530

    Time: 574 ms (74.09%), Space: 368 MB (64.6%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        const int MOD = 1e9 + 7;
+
+        unordered_map<int, long long> left, right;
+        for (int x : nums) right[x]++;
+
+        long long ans = 0;
+
+        for (int j = 0; j < nums.size(); j++) {
+            // current element moves from right to left
+            right[nums[j]]--;
+
+            long long target = 2LL * nums[j];
+
+            if (left.count(target) && right.count(target)) {
+                ans = (ans + left[target] * right[target]) % MOD;
+            }
+
+            left[nums[j]]++;
+        }
+
+        return (int)ans;
+    }
+};
