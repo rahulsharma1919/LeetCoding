8538edffcf980886688f7366d325cbae7ec0837c
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-02-09 13:11:34 +0530

    Time: 0 ms (100%), Space: 14.9 MB (18.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,34 +1,13 @@
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         // traversing till the last element
-//         for (int i = 0; i<nums.size(); i++){
-//             // another loop from i+1 till the last element
-//             for (int j=i+1; j<nums.size(); j++){
-//                 // check if pair's sum is equal to target or not 
-//                 if (nums[i] + nums[j] == target){
-//                     // returning the indices if true 
-//                     return {i,j};
-//                 }
-//             }
-//         }   //brute force method
-//         return {};  // no solution found
-//     }
-// };
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> numMap;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement)) {
-                return {numMap[complement], i};
-            }
-            numMap[nums[i]] = i;
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            int need = target - nums[i];
+            if (mp.count(need))
+                return {mp[need], i};
+            mp[nums[i]] = i;
         }
-
-        return {}; // No solution found
+        return {};
     }
 };
