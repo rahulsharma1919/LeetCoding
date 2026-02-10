4025af69fd5d6242e05e9d6ce5bbcdad509bb919
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-02-10 23:19:32 +0530

    Time: 0 ms (100%), Space: 35.8 MB (92.17%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,5 +1,35 @@
 class Solution {
 public:
+
+    int lowerBound(vector<int>&arr, int x){
+        int s=0, e=arr.size()-1;
+        int ans=e;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid] >= x){
+                ans=mid;
+                e=mid-1;
+            }
+            else if(x>arr[mid]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+    vector<int> binarySearch(vector<int>& arr, int k, int x){
+        int h = lowerBound(arr, x);
+        int l= h-1;
+        while(k--){
+            if(l<0) h++;
+            else if(h>=arr.size()) l--;
+            else if(x-arr[l] > arr[h]-x) h++;
+            else l--;
+        }
+        return vector<int>(arr.begin() + l + 1, arr.begin()+h);
+    }
     vector<int> twoPtrApproach(vector<int>& arr, int k, int x) {
         int l=0, h=arr.size()-1;
         while(h - l >= k){
@@ -13,6 +43,7 @@ public:
         return vector<int>(arr.begin() + l, arr.begin()+h+1);
     }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPtrApproach(arr, k, x);
+        // return twoPtrApproach(arr, k, x);
+        return binarySearch(arr, k, x);
     }
 };
