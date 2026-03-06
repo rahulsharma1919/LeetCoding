fcba736cc79786baf72d5f9c0f149a225889d91b
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-03-06 11:47:09 +0530

    Time: 0 ms (100%), Space: 8.1 MB (96.03%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,4 +1,10 @@
 class Solution {
 public:
-    bool checkOnesSegment(string s) { return s.find("01") == string::npos; }
+    bool checkOnesSegment(string s) {
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i - 1] == '0' && s[i] == '1')
+                return false;
+        }
+        return true;
+    }
 };
