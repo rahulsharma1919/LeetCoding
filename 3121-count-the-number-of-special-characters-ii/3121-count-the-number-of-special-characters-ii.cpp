f10a7329b1cb9475cef7281866e38ad5c3f361f8
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-05-27 12:27:21 +0530

    Time: 61 ms (91.01%), Space: 24.3 MB (68.82%) - LeetHub

---
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        vector<int> lastLower(26, -1), firstUpper(26, INT_MAX);
+
+        for (int i = 0; i < word.size(); i++) {
+            char c = word[i];
+            if (islower(c))
+                lastLower[c - 'a'] = i;
+            else
+                firstUpper[c - 'A'] = min(firstUpper[c - 'A'], i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+            if (lastLower[i] != -1 && firstUpper[i] != INT_MAX &&
+                lastLower[i] < firstUpper[i])
+                ans++;
+        return ans;
+    }
+};
