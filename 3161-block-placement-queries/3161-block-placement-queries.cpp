b1496235cabbe5c8a3c86192003dd6554f51b27c
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-05-30 09:35:14 +0530

    Time: 259 ms (98.02%), Space: 333.9 MB (95.05%) - LeetHub

--- 3161-block-placement-queries/3161-block-placement-queries.cpp
+++ 3161-block-placement-queries/3161-block-placement-queries.cpp
@@ -1,100 +1,34 @@
 class Solution {
 public:
-    vector<int> seg;
-    int N;
-
-    void update(int node, int lo, int hi, int pos, int val) {
-        if (lo == hi) {
-            seg[node] = val;
-            return;
-        }
-        int mid = (lo + hi) / 2;
-        if (pos <= mid)
-            update(2 * node, lo, mid, pos, val);
-        else
-            update(2 * node + 1, mid + 1, hi, pos, val);
-        seg[node] = max(seg[2 * node], seg[2 * node + 1]);
-    }
-
-    int query(int node, int lo, int hi, int l, int r) {
-        if (r < lo || hi < l)
-            return 0;
-        if (l <= lo && hi <= r)
-            return seg[node];
-        int mid = (lo + hi) / 2;
-        return max(query(2 * node, lo, mid, l, r),
-                   query(2 * node + 1, mid + 1, hi, l, r));
-    }
-
-    vector<bool> getResults(vector<vector<int>>& queries) {
-        int maxVal = 0;
-        for (auto& q : queries)
-            maxVal = max(maxVal, q[1]);
-
-        N = maxVal + 1;
-        seg.assign(4 * N, 0);
-
-        set<int> obstacles;
-        obstacles.insert(0);
-        obstacles.insert(maxVal + 1);
-
-        for (auto& q : queries)
+    vector<bool> getResults(vector<vector<int>>& qs) {
+        vector<bool> res;
+        set<int> blocks{0};
+        for (const auto& q : qs)
             if (q[0] == 1)
-                obstacles.insert(q[1]);
-
-        for (auto it = next(obstacles.begin()); it != obstacles.end(); ++it) {
-            auto prev = std::prev(it);
-            if (*it <= maxVal)
-                update(1, 0, N - 1, *it, *it - *prev);
+                blocks.insert(q[1]);
+        for (auto it = next(begin(blocks)); it != end(blocks); ++it)
+            update(*it, *it - *prev(it));
+        for (int i = qs.size() - 1; i >= 0; --i) {
+            int t = qs[i][0], x = qs[i][1], sz = t == 1 ? 0 : qs[i][2];
+            auto it = blocks.lower_bound(x);
+            if (t == 1) {
+                if (next(it) != end(blocks))
+                    update(*next(it), *next(it) - *prev(it));
+                blocks.erase(it);
+            } else
+                res.push_back(x - *prev(it) >= sz || getmax(x) >= sz);
         }
-
-        vector<bool> results;
-
-        set<int> active = {0, maxVal + 1};
-        for (auto it = next(obstacles.begin()); it != obstacles.end(); ++it)
-            if (*it <= maxVal)
-                active.insert(*it);
-
-        set<int> removed;
-        for (int i = (int)queries.size() - 1; i >= 0; i--) {
-            auto& q = queries[i];
-            if (q[0] == 1) {
-                int x = q[1];
-                removed.insert(x);
-                auto it = active.find(x);
-                auto nxt = next(it);
-                auto prv = prev(it);
-                active.erase(it);
-                if (*nxt <= maxVal)
-                    update(1, 0, N - 1, *nxt, *nxt - *prv);
-            }
-        }
-
-        vector<bool> rev;
-        set<int> current = {0, maxVal + 1};
-        seg.assign(4 * N, 0);
-
-        for (auto& q : queries) {
-            if (q[0] == 1) {
-                int x = q[1];
-                auto it = current.insert(x).first;
-                auto nxt = next(it);
-                auto prv = prev(it);
-                update(1, 0, N - 1, x, x - *prv);
-                if (*nxt <= maxVal)
-                    update(1, 0, N - 1, *nxt, *nxt - x);
-            } else {
-                int x = q[1], sz = q[2];
-                auto it = current.upper_bound(x);
-                --it;
-                int lastObs = *it;
-                int tailGap = x - lastObs;
-                int best = lastObs > 0 ? query(1, 0, N - 1, 0, lastObs) : 0;
-                best = max(best, tailGap);
-                rev.push_back(best >= sz);
-            }
-        }
-
-        return rev;
+        return vector<bool>(rbegin(res), rend(res));
+    }
+    int bit[50001] = {};
+    int getmax(int r) {
+        int ret = 0;
+        for (; r >= 0; r = (r & (r + 1)) - 1)
+            ret = max(ret, bit[r]);
+        return ret;
+    }
+    void update(int idx, int val) {
+        for (; idx < 50001; idx = idx | (idx + 1))
+            bit[idx] = max(bit[idx], val);
     }
 };
