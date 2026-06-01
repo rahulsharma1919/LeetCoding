8b7339cf045b380bc832f3bc065e9e239690f647
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-06-01 11:25:28 +0530

    Time: 1 ms (18.16%), Space: 16.9 MB (10.3%) - LeetHub

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(rbegin(cost), rend(cost));
+        int total = 0;
+        for (int i = 0; i < cost.size(); i++)
+            if ((i + 1) % 3 != 0)
+                total += cost[i];
+        return total;
+    }
+};
