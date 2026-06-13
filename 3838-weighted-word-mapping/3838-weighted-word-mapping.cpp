001864ccaeb80ff06b1dd71c0eacecd417a2ee3a
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-06-13 15:46:13 +0530

    Time: 1 ms (37.64%), Space: 43.6 MB (51.01%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string res;
+        for (auto& w : words) {
+            int sum = 0;
+            for (char c : w) sum += weights[c - 'a'];
+            res += (char)('z' - sum % 26);
+        }
+        return res;
+    }
+};
