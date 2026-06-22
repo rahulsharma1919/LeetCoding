d238f46f09ec865adf776205028e5fe2ef17af28
Rahul Sharma <125949979+rahulsharma1919@users.noreply.github.com>
2026-06-22 11:41:38 +0530

    Time: 0 ms (100%), Space: 8.8 MB (95.38%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int freq[26] = {};
+        for (char c : text)
+            freq[c - 'a']++;
+        return min({freq['b' - 'a'], freq['a' - 'a'], freq['l' - 'a'] / 2,
+                    freq['o' - 'a'] / 2, freq['n' - 'a']});
+    }
+};
